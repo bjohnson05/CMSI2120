0ac7b6f9173835915b8e14ac4242d87a2fd393e2
B.J. Johnson <cmsi.bj.lmu@gmail.com>
2021-09-01 14:22:36 -0700

    test commit

---
+++ Classwork/Classwork01/SayHello.java
@@ -0,0 +1,9 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  SayHello.java
+ *  Simple development environment test that uses Java I/O
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+class SayHello {
+   public static void main( String args[] ) {
+      System.out.println( "Hello, world!" );
+   }
+}
