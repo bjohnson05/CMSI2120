896523d9da6ac897983a9612acea3f9bfe3ffe1b
B.J. Johnson <bjohnson05@gmail.com>
2022-08-15 21:41:06 -0700

    remove classwork01 solution

--- Classwork/Classwork01/SayHello.java
+++
@@ -1,9 +0,0 @@
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  SayHello.java
- *  Simple development environment test that uses Java I/O
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-class SayHello {
-   public static void main( String args[] ) {
-      System.out.println( "Hello, world!" );
-   }
-}
